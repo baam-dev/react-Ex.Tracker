8e5c817a0c68185cdebb90d34b268dc2c1719d42
Amir Bakhshi <amirmobili7@gmail.com>
2021-08-04 19:59:17 +0200

    modification

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,7 @@
-// importing the stylesheet, simple
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 ///
-// passing props as argument to the function
-// props will make the inputs dynamic
 function ExpenseItem(props) {
-  //inbuilt methods for the Date()
   return (
     <div className="expense-item">
       {/* here the date has to be passed in as input to next component*/}
