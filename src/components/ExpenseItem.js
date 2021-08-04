f4db58ca0de3cfa51a2afffb7238f0dfebadc8d2
Amir Bakhshi <amirmobili7@gmail.com>
2021-08-04 14:52:59 +0200

    new component(ExpenseDate) added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,20 +1,15 @@
 // importing the stylesheet, simple
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 ///
 // passing props as argument to the function
 // props will make the inputs dynamic
 function ExpenseItem(props) {
   //inbuilt methods for the Date()
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      {/* here the date has to be passed in as input to next component*/}
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
       </div>
