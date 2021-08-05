16aff82ac031a0e86b726972a6c9fb2b2fc485cc
Amir Bakhshi <amirmobili7@gmail.com>
2021-08-05 21:11:44 +0200

    props.children

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,18 @@
+import React from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
+import Card from "../UI/Card";
 ///
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       {/* here the date has to be passed in as input to next component*/}
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
       </div>
       <div className="expense-item__price">${props.amount}</div>
-    </div>
+    </Card>
   );
 }
 
